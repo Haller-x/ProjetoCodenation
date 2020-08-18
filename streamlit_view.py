d327cb6d2b30cdd61dc8e6c0a73feeddd068e8ef
Haller-x <geyoneda@hotmail.com>
2020-08-18 20:13:11 -0300

    fixing issue

--- streamlit_view.py
+++ streamlit_view.py
@@ -31,13 +31,13 @@ def main():
         lista_remover = ['de_faixa_faturamento_estimado','de_faixa_faturamento_estimado_grupo', 'vl_faturamento_estimado_aux','qt_filiais']
         market = market.drop(lista_remover,axis=1)
         chart_options = st.sidebar.multiselect('Escolha os gráficos que gostaria de ver:',market.columns[1::])
-        if chart_options == 0 or chart_options==True:
+        if chart_options:
             for options in chart_options:
                 generate_plot(portifolio_cluster,options)
         st.sidebar.markdown('''---------''')
         st.sidebar.text('Gráficos por Cluster')
         chosen_cluster = st.sidebar.selectbox('Selecione um cluster para avaliar:',portifolio_cluster['Clusters'].value_counts().index,index=0)
-        if chosen_cluster == 0 or chosen_cluster==True:
+        if chosen_cluster == 0 or chosen_cluster:
             df_chosen_cluster = portifolio_cluster.loc[portifolio_cluster.Clusters== chosen_cluster,:]
             charts_clusters = st.sidebar.multiselect('Escolha os gráficos que gostaria de ver:',market.columns[1:-1:])
             if charts_clusters:
